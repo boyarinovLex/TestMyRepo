9c1d69710cd8ac64ad3226cba59959e19fbb36fd
boyarinovLex <boyarinov.lex@ya.ru>
2023-10-28 18:44:58 +0700

    ADD new out String

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.print("У меня всё получилось!");
+		System.out.println(\n "Начинаю потихоньку осваивать Git))")
 	}
 }
